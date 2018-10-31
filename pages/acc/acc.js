8c835108a0e5f3a61cd210001044966e4f959244
QwQ2000 <396707050@qq.com>
2018-10-31 12:55:16 +0800

    Revert "再改了一下acc"
    This reverts commit 72382f8422620b6807e795dfebb9898be8c777ab.

--- pages/acc/acc.js
+++ pages/acc/acc.js
@@ -1,179 +1,90 @@
 // pages/acc/acc.js
 
-
-
-const app = getApp()
-
-
+const app=getApp()
 
 Page({
 
-
-
   /**
-
    * 页面的初始数据1
-
    */
-
   data: {
-
-    getuptime: '6:00',
-
-    name: '你的名字',
-
-    gobedtime: '21:30',
-
-    userInfo: null
-
+    getuptime:'6:00',
+    name:'你的名字',
+    gobedtime:'21:30',
+    userInfo:null
   },
 
-
-
-  changegetuptime: function () { },
-
-  changegobedtime: function () { },
-
-
+changegetuptime:function(){},
+changegobedtime: function () { },
 
   /**
-
    * 生命周期函数--监听页面加载
-
    */
-
   onLoad: function () {
-
     if (app.globalData.userInfo) {
-
       this.setData({
-
-        userInfo: app.globalData.userInfo
-
+        userInfo:app.globalData.userInfo
       })
-
     } else {
-
       app.userInfoReadyCallback = res => {
-
         this.setData({
-
-          userInfo: res.userInfo
-
+         userInfo: res.userInfo
         })
-
       }
-
     }
-
   },
 
-
-
   /**
-
    * 生命周期函数--监听页面初次渲染完成
-
    */
-
   onReady: function () {
 
-
-
   },
 
-
-
   /**
-
    * 生命周期函数--监听页面显示
-
    */
-
   onShow: function () {
-
   },
 
-
-
   /**
-
    * 生命周期函数--监听页面隐藏
-
    */
-
   onHide: function () {
 
-
-
   },
 
-
-
   /**
-
    * 生命周期函数--监听页面卸载
-
    */
-
   onUnload: function () {
 
-
-
   },
 
-
-
   /**
-
    * 页面相关事件处理函数--监听用户下拉动作
-
    */
-
   onPullDownRefresh: function () {
 
-
-
   },
 
-
-
   /**
-
    * 页面上拉触底事件的处理函数
-
    */
-
   onReachBottom: function () {
 
-
-
   },
-
-
 
   /**
-
    * 用户点击右上角分享
-
    */
-
   onShareAppMessage: function () {
-
-
 
   },
 
-
-
   onVersion: function () {
-
     wx.navigateTo({
-
       url: './ver/ver',
-
     })
-
   }
-
 })
