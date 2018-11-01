db75d60924f2dca20afa222c40f2ff9c32b429c9
QwQ2000 <396707050@qq.com>
2018-11-01 19:46:53 +0800

    open-data大法好
    no more getUserInfo

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ App({
           },
           method: "POST",
           success: res2 => {
+            console.log(res2)
             this.globalData.userIds = res2.data
           },
           fail: res3 => {
@@ -40,26 +41,6 @@ App({
           },
           dataType: "sb"
         })
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
       }
     })
   },
