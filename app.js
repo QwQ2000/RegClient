629ea5278eb63e96f6e38f7267382d94993c015d
QwQ2000 <396707050@qq.com>
2018-11-02 23:27:24 +0800

    前后端交互更新
    不仅仅是登录

--- app.js
+++ app.js
@@ -1,51 +1,28 @@
 //app.js
 App({
-  loginRequestCallback: function(c) {
-    wx.request({
-      url: 'http://www.endereyewxy.com/api/login',
-      data: {
-        code: c
-      },
-      method: "POST",
-      success: res2 => {
-        this.globalData.userIds = res2.data
-      },
-      fail: res3 => {
-        loginRequestCallback(c)
-      },
-      dataType: "sb"
-    })
+  globalData: {
+    token: "wxynb",
+    tokenReady: false
   },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
         wx.request({
-          url: 'http://www.endereyewxy.com/api/login',
+          url: 'http://www.endereyewxy.com/api/regserver',
           data: {
-            code: res.code
+            code: res.code,
+            method: "login"
           },
           method: "POST",
           success: res2 => {
-            console.log(res2)
-            this.globalData.userIds = res2.data
+            this.globalData.token=res2.data.token
+            this.globalData.tokenReady=true
           },
           fail: res3 => {
-            loginRequestCallback(res.code)
-          },
-          dataType: "sb"
+            console.log("Login failed")
+          }
         })
       }
     })
-  },
-  globalData: {
-    userInfo: null,
-    userIds: {}
   }
 })
